0735f5ee9b4f8cadfd3ec488ca6b2d1c0684a900
Michael <michael@jasworks.org>
2021-03-01 11:12:53 +0800

    Changed IP address example

--- initial-master-node-setup.sh
+++ initial-master-node-setup.sh
@@ -1,1 +1,1 @@
-sudo kubeadm init --control-plane-endpoint "mnode.jasworks.org" --upload-certs --pod-network-cidr=10.116.0.0/16 --service-cidr=10.16.0.0/12 --apiserver-advertise-address=192.168.238.11 
+sudo kubeadm init --control-plane-endpoint "mnode.jasworks.org" --upload-certs --pod-network-cidr=10.116.0.0/16 --service-cidr=10.16.0.0/12 --apiserver-advertise-address=<IP address of primary interface of your node>
